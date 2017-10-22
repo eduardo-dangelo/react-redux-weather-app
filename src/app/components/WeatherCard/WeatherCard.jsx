f990e007ffb1367ab812372e32355cae57c87384
eduardo-dangelo <eduardodonatodangelo@gmail.com>
2017-10-22 00:15:55 +0000

    final commit

--- src/app/components/WeatherCard/WeatherCard.jsx
+++ src/app/components/WeatherCard/WeatherCard.jsx
@@ -28,12 +28,6 @@ class WeatherList extends Component {
 
     const dayIndex = new Date().getDay();
     const day = dayNames[dayIndex];
-    const temps = cityData.list.map(weather => weather.main.temp);
-    const hours = cityData.list.map(weather => weather.dt_txt.substring(11, 13) + 'h');
-    const key = cityData.list.map((weather, key) => key);
-    const humidities = cityData.list.map(weather => weather.main.humidity);
-    const winds = cityData.list.map(weather => weather.wind.speed);
-    console.log('hours', hours);
 
     const weekDayList = [
       {index: 1, dayNumber: 7},
@@ -42,79 +36,6 @@ class WeatherList extends Component {
       {index: 4, dayNumber: 31},
       {index: 5, dayNumber: 39},
     ];
-
-    const presurreData = {
-      labels: hours,
-      datasets: [
-        {
-          label: 'humidity - %',
-          backgroundColor: 'rgba(50,20,202,0.2)',
-          borderColor: 'rgba(50,20,202,1)',
-          borderWidth: 1,
-          pointRadius: 0,
-          hoverBackgroundColor: 'rgba(50,20,202,0.4)',
-          hoverBorderColor: 'rgba(50,20,202,1)',
-          data: humidities
-        }
-      ]
-    };
-
-    const tempsData = {
-      labels: hours,
-      datasets: [
-        {
-          label: 'temp - celsius',
-          backgroundColor: 'rgba(255,99,132,0.2)',
-          borderColor: 'rgba(255,99,132,1)',
-          borderWidth: 1,
-          pointRadius: 0,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: temps
-        }
-      ]
-    };
-
-    const windData = {
-      labels: hours,
-      datasets: [
-        {
-          label: 'wind - kmh',
-          backgroundColor: 'rgba(62,62,62,0.2)',
-          borderColor: 'rgba(62,62,62,1)',
-          borderWidth: 1,
-          pointRadius: 0,
-          hoverBackgroundColor: 'rgba(62,62,62,0.4)',
-          hoverBorderColor: 'rgba(62,62,62,1)',
-          data: winds
-        }
-      ]
-    };
-
-    const options = {
-      maintainAspectRatio: false,
-      scales: {
-        yAxes: [{
-          stacked: true,
-          gridLines: {
-            display: true,
-            color: "rgba(255,99,132,0.2)"
-          }
-        }],
-        xAxes: [{
-          gridLines: {
-            display: false
-          }
-        }]
-      }
-    };
-
-    const chartList = [
-      {name: tempsData, color: 'green'},
-      {name: presurreData, color: 'green'},
-      {name: windData, color: 'green'},
-    ];
-
     
     console.log(flipCard);
     return (
